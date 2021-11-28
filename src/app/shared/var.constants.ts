2dfa417f64b9f39ec0e9f386b27239351c5315e5
Luis Angel Mendoza <94770111+EDWINMENENDEZ@users.noreply.github.com>
2021-11-27 23:50:47 -0600

    cambios
    estos cambios ya son para poder que se compile  en heroku

--- src/app/shared/var.constants.ts
+++ src/app/shared/var.constants.ts
@@ -1,2 +1,2 @@
-export const URL_HOST= 'http://localhost:3000/api/';
-//export const URL_HOST= ''
+//export const URL_HOST= 'http://localhost:3000/api/';
+export const URL_HOST= 'https://multiservicios-api.herokuapp.com/api/'
